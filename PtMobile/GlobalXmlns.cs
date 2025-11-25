77e0ee8864e3fe1d3ad513f4704bded93318d850
User <User@MSI>
2025-11-25 10:03:11 +0800

    Add project files.

---
+++ PtMobile/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PtMobile")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "PtMobile.Pages")]
---
+++ PtMobile/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace PtMobile;
+
+public partial class MainPage : ContentPage
+{
+    public ObservableCollection<ItemModel> Items { get; set; }
+
+    public MainPage()
+    {
+        InitializeComponent();
+
+        Items = new ObservableCollection<ItemModel>
+        {
+            new ItemModel { Title = "A" },
+            new ItemModel { Title = "B" },
+            new ItemModel { Title = "C" },
+            new ItemModel { Title = "D" }
+        };
+
+        BindingContext = this;
+    }
+}
+
+public class ItemModel
+{
+    public string Title { get; set; }
+}
